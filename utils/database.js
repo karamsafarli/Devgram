53a2cbcf5acaea0754485e603da6369ca87e35ed
karamsafarli <karamslee007@gmail.com>
2023-06-17 16:33:43 +0400

    Error fixed

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGODB_URI.toString(), {
             dbName: 'devgram',
             useNewUrlParser: true,
             useUnifiedTopology: true
